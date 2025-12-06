c15c348d7bcf0b8c908a24ef8d59d814b98b01c5
fureev <fureev@gmail.com>
2025-12-07 01:56:24 +0200

    fix

--- src/logger.go
+++ src/logger.go
@@ -1,12 +1,12 @@
 package parallel
 
 import (
-    "fmt"
-    "hash/fnv"
-    "sync"
-    "time"
+	"fmt"
+	"hash/fnv"
+	"sync"
+	"time"
 
-    "github.com/efureev/reggol"
+	"github.com/efureev/reggol"
 )
 
 var (
@@ -35,28 +35,31 @@ func createLogger() *reggol.Logger {
 }
 
 func createTransformer() *reggol.ConsoleTransformer {
-    trans := reggol.NewConsoleTransformer(false, time.TimeOnly)
+	trans := reggol.NewConsoleTransformer(false, time.TimeOnly)
 
-    colorList := GenColors(true)
+	colorList := GenColors(true)
 
-    trans.FormatFieldFn = func(i interface{}) string {
-        // Safe type handling to avoid panics on unexpected input.
-        switch list := i.(type) {
-        case [2]string:
-            // Pick a color deterministically by hashing the key to avoid any shared mutable state.
-            // This makes the transformer concurrency-safe across goroutines.
-            h := fnv.New32a()
-            _, _ = h.Write([]byte(list[0]))
-            idx := int(h.Sum32()) % len(colorList)
-            if idx < 0 {
-                idx = -idx
-            }
-            currColor := colorList[idx]
-            return fmt.Sprintf(`%s%s`, reggol.SetColor(list[0]+`=`, currColor, trans.IsNoColor()), list[1])
-        default:
-            return fmt.Sprintf(`%v`, i)
-        }
-    }
+	trans.FormatFieldFn = func(i interface{}) string {
+		// Safe type handling to avoid panics on unexpected input.
+		switch list := i.(type) {
+		case [2]string:
+			// Pick a color deterministically by hashing the key to avoid any shared mutable state.
+			// This makes the transformer concurrency-safe across goroutines.
+			h := fnv.New32a()
+			_, _ = h.Write([]byte(list[0]))
 
-    return &trans
+			idx := int(h.Sum32()) % len(colorList)
+			if idx < 0 {
+				idx = -idx
+			}
+
+			currColor := colorList[idx]
+
+			return fmt.Sprintf(`%s%s`, reggol.SetColor(list[0]+`=`, currColor, trans.IsNoColor()), list[1])
+		default:
+			return fmt.Sprintf(`%v`, i)
+		}
+	}
+
+	return &trans
 }
